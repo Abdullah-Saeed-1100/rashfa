b36d48417d5c62424190996cbb4171b58a8ddeeb
Abdullah Bagar <abdullah.saeed.bagar@gmail.com>
2025-06-22 05:30:35 +0300

    refactor ItemCard widget

--- lib/core/widgets/item_card.dart
+++ lib/core/widgets/item_card.dart
@@ -13,6 +13,7 @@ class ItemCard extends StatelessWidget {
     return Container(
       padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
       // margin: const EdgeInsets.symmetric(horizontal: 12, vertical: 8),
+      clipBehavior: Clip.hardEdge,
       decoration: BoxDecoration(
         borderRadius: BorderRadius.circular(20),
         // same color background in theme
@@ -26,21 +27,20 @@ class ItemCard extends StatelessWidget {
         ],
       ),
       // ================  content for Container ===============
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          // ================  image ===============
-          InkWell(
-            onTap: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder:
-                      (context) => DetailsJuiceScreen(juiceModel: juiceModel),
-                ),
-              );
-            },
-            child: Center(
+      child: InkWell(
+        onTap: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => DetailsJuiceScreen(juiceModel: juiceModel),
+            ),
+          );
+        },
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            // ================  image ===============
+            Center(
               child: Image.network(
                 // juiceModel.imageUrl,
                 "https://res.cloudinary.com/dm9jjz6qz/image/upload/v1750492056/Espresso_jgwt1g.png",
@@ -51,64 +51,64 @@ class ItemCard extends StatelessWidget {
                 fit: BoxFit.contain,
               ),
             ),
-          ),
-          SizedBox(height: 10),
-          // ================  Name Product ===============
-          Text(
-            juiceModel.name,
-            style: const TextStyle(
-              fontSize: 18,
-              fontWeight: FontWeight.bold,
-              color: Colors.white,
+            SizedBox(height: 10),
+            // ================  Name Product ===============
+            Text(
+              juiceModel.name,
+              style: const TextStyle(
+                fontSize: 18,
+                fontWeight: FontWeight.bold,
+                color: Colors.white,
+              ),
             ),
-          ),
-          //
-          const SizedBox(height: 8),
-          // ==============   Description Product =============
-          Text(
-            juiceModel.description,
-            style: const TextStyle(fontSize: 14, color: Colors.white60),
-            maxLines: 2,
-            overflow: TextOverflow.ellipsis,
-          ),
-          //
-          // const SizedBox(height: 13),
-          Spacer(),
-          // ================  row Price and Add button ===============
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Container(
-                padding: const EdgeInsets.all(5),
-                decoration: BoxDecoration(
-                  color: const Color(0xFFE57734),
-                  borderRadius: BorderRadius.circular(10),
+            //
+            const SizedBox(height: 8),
+            // ==============   Description Product =============
+            Text(
+              juiceModel.description,
+              style: const TextStyle(fontSize: 14, color: Colors.white60),
+              maxLines: 2,
+              overflow: TextOverflow.ellipsis,
+            ),
+            //
+            // const SizedBox(height: 13),
+            Spacer(),
+            // ================  row Price and Add button ===============
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Container(
+                  padding: const EdgeInsets.all(5),
+                  decoration: BoxDecoration(
+                    color: const Color(0xFFE57734),
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: const Text(
+                    "أضف الى السلة",
+                    style: TextStyle(
+                      fontSize: 11.5,
+                      // fontWeight: FontWeight.w500,
+                      color: Colors.white,
+                    ),
+                  ),
+                  // const Icon(
+                  //   Icons.add,
+                  //   size: 20,
+                  //   color: Colors.white,
+                  // ),
                 ),
-                child: const Text(
-                  "أضف الى السلة",
-                  style: TextStyle(
-                    fontSize: 11.5,
-                    // fontWeight: FontWeight.w500,
+                Text(
+                  "\$${juiceModel.price}",
+                  style: const TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.w500,
                     color: Colors.white,
                   ),
                 ),
-                // const Icon(
-                //   Icons.add,
-                //   size: 20,
-                //   color: Colors.white,
-                // ),
-              ),
-              Text(
-                "\$${juiceModel.price}",
-                style: const TextStyle(
-                  fontSize: 18,
-                  fontWeight: FontWeight.w500,
-                  color: Colors.white,
-                ),
-              ),
-            ],
-          ),
-        ],
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
